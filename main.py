c3d41b1f12deb617dbe7a5d68955762e882208a9
Андрій Комендант <imssskel@gmail.com>
2025-04-05 13:27:50 +0300

    not second commit

--- main.py
+++ main.py
@@ -69,6 +69,57 @@ for link in links:
                     title.text = title_li
 
 
+link = 'https://wiki.factorio.com/Template:TechNav'
+response = requests.get(link).text
+soup = BeautifulSoup(response, "lxml")
+
+
+block = soup.find('div', {'id': "mw-content-text"})
+tag_table = block.find('table', {'class': "navbox-inner"})
+tag_tr = tag_table.find_all('tr')
+
+for tag_tr_iter in tag_tr:
+    tag_td_gd = tag_tr_iter.find('td', {'class': "group-data"})
+    tag_td_ld = tag_tr_iter.find('td', {'class': "list-data"})
+
+    if tag_td_gd:
+        tag_p = tag_td_gd.find('p')
+        if tag_p and tag_p.text.strip() == 'Navigation':
+            continue
+        else:
+            div_tag = tag_td_gd.find('div')
+            if div_tag:
+                title_div = div_tag.text.strip()
+                print(Fore.GREEN + title_div + ':')
+                if sub_category is not None:
+                    sub_category = None
+                sub_category = ET.SubElement(category, 'sub_category')
+                sub_category.set('name', title_div)
+    else:
+        tag_th = tag_tr_iter.find('th', {'class': "navbox-header"})
+        if tag_th:
+            div_tag = tag_th.find('div')
+            if div_tag:
+                title_div = div_tag.text.strip()
+                print(Fore.RED + '-----' + title_div + '-----')
+                category = ET.SubElement(root, 'category')
+                category.set('name', title_div)
+
+    if tag_td_ld:
+        li_tag = tag_td_ld.find_all('li')
+        for li_tag_iter in li_tag:
+            title_li = li_tag_iter.find('a').get('title', '')
+            print(title_li)
+            if sub_category is not None:
+                title = ET.SubElement(sub_category, 'title')
+                title.text = title_li
+
+tree = ET.ElementTree(root)
+with open("library.xml", "wb") as file:
+    tree.write(file)
+
+
+
 def prettify(elem):
     from xml.dom import minidom
     rough_string = ET.tostring(elem, 'utf-8')
